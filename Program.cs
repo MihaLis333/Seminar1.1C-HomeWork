caf3472dd7decaa4d31326daa45e35cccbf3a8f9
MihaLis333 <m190333@ya.ru>
2022-12-02 16:58:31 +0300

    добавлена задача 2

--- Program.cs
+++ Program.cs
@@ -14,6 +14,30 @@ else
 {
     Console.WriteLine($"Больше число: " + num2);
 }
-/*
+*/
 
 // Задача 2. Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+/*
+Console.Write("Введите первое число!: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число!: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число!: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int Max(int num1, int num2, int num3)
+{
+    int result = num1;
+    if(num2 > result ) result = num2;
+    if(num3 > result ) result = num3;
+    return result;
+}
+
+int max1 = Max(num1, num2, num3);
+
+Console.WriteLine(max1);
+*/
+
+// Задача 3. Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
